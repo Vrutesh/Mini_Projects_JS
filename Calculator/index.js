aaf6c6cb25e5b39d48d26ec17124a16cfeeb2af8
Vrutesh Mayekar <vruteshpm02@gmail.com>
2024-02-20 11:32:53 +0530

    added file structure

---
+++ Calculator/index.js
@@ -0,0 +1,15 @@
+function clear_Display() {
+    document.getElementById("display").value = "";
+}
+let buttons = document.getElementsByTagName("button").value
+
+function appendtoDisplay(){
+    // buttons.forEach(button => {
+        console.log(buttons)
+        // button.value = document.getElementById("display")
+    // });
+}
+
+function calculate(){
+    
+}
